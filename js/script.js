95697ffbfe91b740404c62770375e48cf26a7cad
thuhuynh4393 <thuhuynh4393@gmail.com>
2016-06-04 20:53:37 +0700

    my personal page

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//   $('.slider').slick({
+//     prevArrow: $('.btn-pre'),
+//     nextArrow: $('.btn-next'),
+//     dots: true,
+//   });
+// });
+
+// $('a').smoothScroll();
+$('.slider').slick({
+  dots: true,
+  focusOnSelect: true,
+  prevArrow: $('.btn-pre'),
+  nextArrow: $('.btn-next'),
+  autoplay: true
+});
+
+
